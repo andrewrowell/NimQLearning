2042495b6f4a532e9a0e28e13c71aa2759af501a
Andrew Rowell <andrewjrowell@gmail.com>
2020-05-18 11:22:22 -0500

    Fixed graph, added docs, updated readme.

--- README.md
+++ README.md
@@ -6,6 +6,31 @@ All the bot knows before learning:
 * What moves it can make
 * How many sticks are on the board
 
-At the end of the game it receives a positive or negative reward, and then applies that reward to its experience in the game to learn which moves are best depending on what the state of the game is.
+At the end of the game it receives a positive or negative reward, and then
+applies that reward to its experience in the game to learn which moves are
+best depending on what the state of the game is.
 
-Fairly quickly, it can learn the game well enough to be impossible to beat.
+Because of the deterministic nature of Nim, it can quickly learn the game well
+ enough to be impossible to beat.
+ 
+## How to Run
+
+You'll need matplotlib. To see how it plays against an opponent that chooses
+random actions, you can just run `nim.py`. It will generate a chart,
+`result.png`, that will show its win rate (using a rolling average).
+
+You can find code at the bottom of `nim.py` that can be modified so that you
+can play against the learner. If you let it play against the random opponent
+first, you will lose, so turn that off if you want to see it learn the game
+against you.
+ 
+## Files
+* `nim.py` - Play a number of games. Can be Q-Learner vs Random or Q-Learner vs
+Human.
+* `web/qnim.html` and `web/qnim.js` - In progress, javascript Q-Learner (for demo on web).
+
+## To Do
+* Finish javascript implementation
+* Split classes into separate files
+  * Learners.py, Game.py, etc. ?
+* Add other learners (using neural net rather than table?)
--- nim.py
+++ nim.py
@@ -1,41 +1,94 @@
 import random
 import matplotlib.pyplot as plt
 
-GRAPH_SMOOTHING = 3
+# Window size for rolling average in win rate graph
+GRAPH_SMOOTHING = 5
 
+# Number of sticks
 GAME_SIZE = 10
-ACTION_SPACE = [i for i in range(1,4)]
+
+# Max number of sticks that can be removed in a single turn
+MAX_STICKS_REMOVABLE = 3
+
+ACTION_SPACE = [i for i in range(1,MAX_STICKS_REMOVABLE + 1)]
 
 class Game:
+    '''
+    Keeps track of the game state.
+    '''
     def __init__(self):
+        '''
+        Sets the initial number of sticks.
+        '''
         self.sticks = GAME_SIZE
 
     def remove(self, count):
+        '''
+        Remove a number of sticks from the board.
+        :param count: Number of sticks to remove.
+        '''
         self.sticks -= count
 
     def getState(self):
+        '''
+        Converts the number of sticks to a "state id" for the learner.
+        :return: state id.
+        '''
         return self.sticks - 1
 
     def getActionSpace(self):
+        '''
+        :return: Possible actions in the game.
+        '''
         return ACTION_SPACE
 
     def isOver(self):
+        '''
+        :return: True if all sticks have been removed and the game is "done".
+        '''
         return self.sticks <= 0
 
-# Q Learning Agent
 class QLearner:
+    '''
+    Tabular Q Learner.
+
+    Keeps track of every possible state of the game, and determines the
+    expected value of any move in any state. Eventually builds up enough
+    data to choose the optimal action in any state.
+
+    The learner is not aware of the rules, goals, or other player's
+    decision process. It operates solely based on game state, available
+    actions, and positive/negative rewards depending on win/loss.
+    '''
+
     def __init__(self):
+        '''
+        Initialize the Q-Table
+        '''
         self.q = []
         for i in range(GAME_SIZE):
             self.q.append([0] * len(ACTION_SPACE))
 
     def getMove(self, state):
-        # Pick action that the agent remembers having the best reward, on average.
+        '''
+        Pick action with the best expected value.
+        :param state: Game state.
+        :return: Chosen action.
+        '''
         potential_actions = self.q[state]
         action_chosen = potential_actions.index(max(potential_actions))
         return action_chosen
 
     def learn(self, state, action, new_state, reward, is_over):
+        '''
+        Add new information about the game to the table.
+
+        :param state: Original state before move.
+        :param action: Move chosen.
+        :param new_state: State of game after opponent's move.
+        :param reward: Reward to assign to the action.
+        :param is_over: Whether the game is over or not.
+        '''
         if is_over:
             # If the game was over, new_state is not a valid state.
             # Remembers that doing "action" in "state" gave some reward.
@@ -45,19 +98,27 @@ class QLearner:
             # (whose value we may have an idea of) and gave some reward.
             self.q[state][action] = reward + sum(self.q[new_state]) / len(self.q[new_state])
 
-# Opponent that just takes random actions.
 class RandomOpponent:
+    '''
+    Opponent that just takes random actions.
+    '''
+
     def __init__(self):
         return
 
     def getMove(self, state):
+        '''
+        Choose random action from the action space.
+        '''
         return random.choice(Game().getActionSpace())
 
     def learn(self, state, action, new_state, reward, is_over):
         return
 
-# Opponent that lets a human decide the moves.
 class HumanOpponent:
+    '''
+    Opponent that lets a human decide the moves.
+    '''
     def __init__(self):
         return
 
@@ -68,6 +129,16 @@ class HumanOpponent:
         return
 
 def runTrial(learner, number_of_trials, opponent_learner_type, verbose, make_graph, print_q_values):
+    '''
+    Play multiple games.
+    :param learner: Player that learns.
+    :param number_of_trials:
+    :param opponent_learner_type:
+    :param verbose:
+    :param make_graph:
+    :param print_q_values:
+    :return:
+    '''
     q_wins = []
 
     opponent = opponent_learner_type()
@@ -131,14 +202,15 @@ def runTrial(learner, number_of_trials, opponent_learner_type, verbose, make_gra
             q_win_x.append(x)
         plt.plot(q_win_x, q_win_y, label="Q-Learner Win Rate")
         plt.fill_between(q_win_x, 0, q_win_y, label="Q-Learner Win Rate")
-        plt.show()
+        plt.title("Win Rate (Rolling Window Size = " + str(GRAPH_SMOOTHING) +" Games)")
+        plt.savefig("result.png")
 
 
 
 learner = QLearner()
 
 # Train against a random opponent.
-#runTrial(learner, 100, RandomOpponent, False, True, False)
+runTrial(learner, 100, RandomOpponent, False, True, False)
 
 # Let human play against it.
-runTrial(learner, 1000, HumanOpponent, True, True, False)
+#runTrial(learner, 5, HumanOpponent, True, True, True)
